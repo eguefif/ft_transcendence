673be00dbf34e26020e6316f8b83e273239df4b8
eguefif <eguefif@fastmail.com>
2024-02-07 14:07:45 -0500

    Deactivated html renderer in views

--- django/transcendence/register/urls.py
+++ django/transcendence/register/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from register import views
 
 urlpatterns = [
-        path('dj/register/', views.registerUser),
+        path('dj/register/', views.RegisterView.as_view()),
         ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
