8b389179700160d31e1af74451a252022ab4058e
eguefif <eguefif@fastmail.com>
2024-02-09 16:28:22 +0000

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,11 @@ mv ./.docker-compose-deploy.yml ./.docker-compose.yml
 
 docker-compose up --build -d
 
+mv ./django/.Docker-dev .django/Docker
+mv ./django/Docker ./django/.Docker-deploy
 
+mv ./nginx/Docker ./nginx/.Docker-deploy
+mv ./nginx/.Docker-dev ./nginx/Docker
+
+mv ./docker-compose.yml ./.docker-compose-deploy.yml
+mv ./.docker-compose-dev.yml ./.docker-compose.yml
