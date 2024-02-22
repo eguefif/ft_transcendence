41a629e8580b758932c9d979174a1f9bb86ce0ac
Maxil Cote <macote@c1r7p9.42quebec.com>
2024-02-22 13:45:59 -0500

    added showBoard() and hideBoard()

--- nginx/www/dev/scripts/modules/Game.js
+++ nginx/www/dev/scripts/modules/Game.js
@@ -1,13 +1,18 @@
 import { graphicEngine } from "./graphic-engine.js"
+import { renderer } from "./graphic-engine.js"
+
 
 export class Game {
 	constructor(controller) {
+		renderer.showBoard()
+		renderer.hideBoard()
 		this.controller = controller
 		this.graphicEngine = new graphicEngine()
 		this.running = true
 		let menu = document.querySelector("#menubtn")
 		if (menu != undefined) {
 			menu.addEventListener("click", (e) => {
+				// renderer.hideBoard()
 				this.running = false
 				this.controller.cleanup()
 				this.controller.stop = true
