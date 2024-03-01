d09f70e211cdf11504d352382b90fdce37246278
eguefif <eguefif@fastmail.com>
2024-03-01 08:40:35 -0500

    Added homemade healthcheck for django in nginx

--- nginx/tools/entry-point.sh
+++ nginx/tools/entry-point.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
+for i in {1..30}
+do
+	if curl --silent http://django:8000/healthcheck/; then
+		break
+	fi
+	sleep 1
+done
+
+if [ $i = 30 ]; then
+	echo "connection with django failed: " $i
+fi
+
 if [ ! -d "/etc/ssl/certs" ]; then
 	mkdir /etc/ssl/certs
 fi
