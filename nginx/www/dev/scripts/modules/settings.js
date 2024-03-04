93654529c8c76ef540f703d23b6fd2a3db7262e9
PelletierM <122843454+PelletierM@users.noreply.github.com>
2024-03-04 16:31:39 -0500

    Add frontend validation of form

--- nginx/www/dev/scripts/modules/settings.js
+++ nginx/www/dev/scripts/modules/settings.js
@@ -1,7 +1,14 @@
 import { fetcher } from "./fetcher.js"
-import { clearContent, createAlert, closeCovering } from "./utils.js"
+import { clearContent, sendAlert, closeCovering } from "./utils.js"
 import { closeModal } from "./modalConnection.js"
 import { removeModal } from "./navbar.js"
+import { getSVG } from "./iconSVG.js"
+
+export async function generateSettings() {
+	removeModal()
+	document.querySelector('body').appendChild(createModalSettings())
+	await initSettings();
+}
 
 async function preview_image() {
     const validation = document.getElementById("pictureUploadValidation")
@@ -29,15 +36,21 @@ async function preview_image() {
     }
 }
 
-export function initSettings() {
-    const profileImageField = document.getElementById("profileImageField")
+async function initSettings() {
+//    const profileImageField = document.getElementById("profileImageField")
 
-    profileImageField.addEventListener("change", preview_image);
+//   profileImageField.addEventListener("change", preview_image);
+	initProfileInfoCard();
+	await updateUserData();
 }
 
-
-async function profileInfo()
-{
+async function updateUserData() {
+	const res = await fetcher.get("api/profile/userinfo/")
+	if (res.status >= 200 && res.status < 300) {
+		updateProfileUsername(res.data);
+		updateProfileInfo(res.data);
+		updatePassAuth(res.data);
+	}
 	const imgElement = document.getElementById("profilePicture")
 	const imageReply = await fetcher.get("api/profile/userpicture/")
 	if (imageReply.status >= 200 && imageReply.status < 300 && imageReply.status != 202) {
@@ -50,76 +63,43 @@ async function profileInfo()
 	else {
 		imgElement.src = "images/avatar.png"
 	}
-	const res = await fetcher.get("api/profile/userinfo/")
-	if (res.status >= 200 && res.status < 300) {
-		const mailProfile = document.querySelector("#profileEmail")
-		if (mailProfile)
-			mailProfile.value = res.data['email']
-		const firstName = document.querySelector("#profileFirstName");
-		if (firstName && res.data.first_name)
-			firstName.value = res.data.first_name;
-		const lastName = document.querySelector("#profileLastName");
-		if (lastName && res.data.last_name)
-			lastName.value = res.data.last_name;
-	}
-}
-
-function authUpdateProfile()
-{
-	const modifyProfileForm = document.querySelector("#profileForm")
-	const btnCancel = document.getElementById("cancelButton")
-
-	btnCancel.addEventListener("click", function (e) {
-		e.preventDefault()
-		removeCardOrForm()
-		addCard()
-	})
-
-	modifyProfileForm.addEventListener("submit", function (e) {
-		e.preventDefault()
-		const data = new FormData(e.target)
-		const url = e.target.action
-		const body = {
-			'email': data.get('email'),
-			'first_name': data.get('firstName'),
-			'last_name': data.get('lastName'),
-		}
-		sendUpdateProfileRequest(url, body)
-		removeCardOrForm()
-		addCard()
-	})
 }
 
 async function sendUpdateProfileRequest(url, body)
 {
-	const profileEmail = document.querySelector("#profileEmail")
-	const profileEmailValidation = document.querySelector("#profileEmailValidation")
-
-	const form = document.getElementById("profileForm")
-	resetFormInput(form)
-
 	const res = await fetcher.post(url, body)
-	if (res.status == 201)
+	if (res.status >= 200 && res.status < 300)
 	{
-		const settingsModal = document.querySelector("#settingsModal")
-		const alert = createAlert('success', 'Your information has been saved')
-		const siblingElement = settingsModal.firstChild;
-		settingsModal.insertBefore(alert, siblingElement)
-		setTimeout(() => {
-			const bsAlert = new bootstrap.Alert(alert);
-			bsAlert.close()
-		}, 3000);
-
+		const form = document.getElementById("profileForm")
+		const alert = sendAlert('success', 'Your information has been saved', 3000)
+		updateUserData();
+		closeCovering(form.parentElement);
 	}
-	else if (res.status == 400)
+	else if (res.status >= 400 && res.status < 500)
 	{
-		const data = res.data
-
-		if (data['email'])
+		const firstNameField = document.getElementById("info-form-first-name");
+		const firstNameValidation = document.getElementById("info-form-first-name-validation");
+		const lastNameField = document.getElementById("info-form-last-name");
+		const lastNameValidation = document.getElementById("info-form-last-name-validation");
+		const emailField = document.getElementById("info-form-email");
+		const emailValidation = document.getElementById("info-form-email-validation");
+		if (res.data['first_name'])
 		{
-			profileEmail.classList.add("is-invalid")
-			profileEmailValidation.classList.add("invalid-feedback")
-			profileEmailValidation.innerHTML = data['email']
+			firstNameField.classList.add("is-invalid")
+			firstNameValidation.classList.add("invalid-feedback")
+			firstNameValidation.innerText = res.data['first_name']
+		}
+		if (res.data['last_name'])
+		{
+			lastNameField.classList.add("is-invalid")
+			lastNameValidation.classList.add("invalid-feedback")
+			lastNameValidation.innerText = res.data['last_name']
+		}
+		if (res.data['email'])
+		{
+			emailField.classList.add("is-invalid")
+			emailValidation.classList.add("invalid-feedback")
+			emailValidation.innerText = res.data['email']
 		}
 	}
 }
@@ -128,13 +108,14 @@ function resetFormInput(form) {
 	const inputs = form.querySelectorAll("input")
 	inputs.forEach(input => {
 		input.classList.remove("is-invalid")
-		input.classList.add("is-valid")
 	})
 }
 
 function createModalSettings() {
-	return `
-	<div class="modal fade" id="settingsModal">
+	let settingsModal = document.createElement("div");
+	settingsModal.classList.add("modal", "fade");
+	settingsModal.id = "settingsModal";
+	settingsModal.innerHTML = `
 		<div class="modal-dialog">
 			<div class="modal-content bg-dark">
 				<div class="modal-header">
@@ -143,74 +124,79 @@ function createModalSettings() {
 				</div>
 				<div class="modal-body">
 					<div>
-						<h5>Password and authentication</h3>
-						<div id="pass-change">
-							<a>Change password</a>
+						<div id="profile-main" class="d-flex flex-column align-items-center">
+							<div id="profile-picture-container">
+								<img id="profilePicture" class="rounded-circle-border mx-auto d-block" alt="profilePicture"/>
+								${getSVG.formSVG.updatePicture}
+							</div>
+							<span id="profile-main-username" class="fs-1"></span>
 						</div>
-						<div id="auth-type" class="d-flex flex-column">
-							<span class="fw-bold">Account type</span>
-							<span class="user-dependent"></span>
+						<hr style="width: 25%">
+						<div id="profile-info">
+							<h5>Profile info</h5>
+							<div id="info-first-name" class="d-flex flex-column">
+								<span class="fw-bold">First Name</span>
+								<span class="user-dependent"></span>
+							</div>
+							<div id="info-last-name" class="d-flex flex-column">
+								<span class="fw-bold">Last Name</span>
+								<span class="user-dependent"></span>
+							</div>
+							<div id="info-email" class="d-flex flex-column">
+								<span class="fw-bold">Email</span>
+								<span class="user-dependent"></span>
+							</div>
+							<a id="info-edit-button" class="btn btn-primary">edit</a>
 						</div>
-						<div id="otp-toggle">
-							<span class="fw-bold">Two-factor authentication</span>
-							<div class="user-dependent"></div>
+						<hr style="width: 25%">
+						<div id="pass-auth">
+							<h5>Password and authentication</h5>
+							<div id="pass-change">
+								<a>Change password</a>
+							</div>
+							<div id="auth-type" class="d-flex flex-column">
+								<span class="fw-bold">Account type</span>
+								<span class="user-dependent"></span>
+							</div>
+							<div id="otp-toggle">
+								<span class="fw-bold">Two-factor authentication</span>
+								<div class="user-dependent"></div>
+							</div>
 						</div>
 					</div>
 				</div>
-			  </div>
 			</div>
 		</div>
-	</div>
 	`
+	return settingsModal;
 }
 
-function createformSettings() {
-	return `
-	<form id="profileForm" action="api/profile/updateprofile/" method="PATCH">
-		<img id="profilePicture" class="rounded-circle-border mx-auto d-block" alt="profilePicture"/>
-		<div id="profileImageContainer" class="mb-3">
-			<label for="profileImageField" class="form-label">Profile picture</label>
-			<input class="form-control" type="file" id="profileImageField">
-			<div id="pictureUploadValidation" class="error text-danger"></div>
-		</div>
-		<div class="mb-3">
-			<label for="profileEmail" class="col-md-3 col-form-label">Email</label>
-			<input type="email" name="email" id="profileEmail" class="form-control" placeholder="email">
-			<div id="profileEmailValidation"></div>
-		</div>
-		<div class="mb-3">
-			<label for="profileFirstName" class="col-md-3 col-form-label">First Name</label>
-			<input type="text" name="firstName" id="profileFirstName" class="form-control" placeholder="First Name">
-			<div id="profileFirstNameValidation"></div>
-		</div>
-		<div class="mb-3">
-			<label for="profileLastName" class="col-md-3 col-form-label">Last Name</label>
-			<input type="text" name="lastName" id="profileLastName" class="form-control" placeholder="Last Name">
-			<div id="profileLastNameValidation"></div>
-		</div>
-		<button type="submit" class="btn btn-primary" id="profileSaveChanges">Save changes</button>
-		<button type="button" class="btn btn-light" id="cancelButton">Cancel</button>
-		<a>Change password</a>
-	</form>
-	`
+// PROFILE MAIN
+function updateProfileUsername(data) {
+	const usernameField = document.getElementById("profile-main-username");
+	if (data.username) {
+		usernameField.innerText = data.username;
+	}
 }
 
-function createCardSettings() {
-	return `
-	<div class="card bg-dark text-center">
-		<img id="profilePicture" class="rounded-circle-border mx-auto d-block" alt="profilePicture"/>
-		<div class="card-body d-flex flex-column align-items-center" >
-			<span class="userName fs-1">Le Loup de WallStreet !</span>
-			<span class="lastName fs-5">Leloup</span>
-			<span class="firstName fs-5">Jean</span>
-			<span class="cardEmail fs-5">pierre@gmail.com</span>
-			<a id="editButton" class="btn btn-primary">edit</a>
-		</div>
-	</div>
-	`
+// PROFILE INFO
+
+function updateProfileInfo(data) {
+	const emailProfile = document.getElementById("info-email").querySelector(".user-dependent");
+	if ("email" in data) {
+		emailProfile.innerText = data.email
+	}
+	const firstName = document.getElementById("info-first-name").querySelector(".user-dependent");
+	if ("first_name" in data) {
+		firstName.innerText = data.first_name;
+	}
+	const lastName = document.getElementById("info-last-name").querySelector(".user-dependent");
+	if ("last_name" in data) {
+		lastName.innerText = data.last_name;
+	}
 }
 
-async function editProfileCard() {
+async function editProfileInfoCard() {
 	const imgElement = document.getElementById("profilePicture")
 	const imageReply = await fetcher.get("api/profile/userpicture/")
 	if (imageReply.status == 200) {
@@ -241,43 +227,156 @@ async function editProfileCard() {
 	}
 }
 
-function removeCardOrForm() {
-	const cardSettings = document.querySelector('.card')
-	const formSettings = document.getElementById('profileForm')
-	if (cardSettings) {
-		cardSettings.remove()
-	}
-	if(formSettings) {
-		formSettings.remove()
-	}
-}
-
-async function addCard () {
+async function initProfileInfoCard() {
 	const modalBody = document.querySelector('.modal-body')
-	modalBody.insertAdjacentHTML("afterbegin", createCardSettings())
-	const buttonEdit = document.getElementById("editButton")
-	await editProfileCard()
-	buttonEdit.addEventListener("click", async e  => {
-		e.preventDefault()
-		removeCardOrForm()
-		addChangeSettings()
+	const buttonEdit = document.getElementById("info-edit-button")
+	buttonEdit.addEventListener("click", (e) => {
+		initProfileInfoForm();
 	});
 }
 
 function addChangeSettings () {
 	const modalBody = document.querySelector('.modal-body')
-	modalBody.insertAdjacentHTML("afterbegin", createformSettings())
+	modalBody.insertAdjacentHTML("afterbegin", createProfileInfoForm())
 	profileInfo()
 	initSettings();
 	authUpdateProfile()
 }
 
-export function generateSettings() {
-	removeModal()
-	document.querySelector('nav').insertAdjacentHTML('afterend', createModalSettings())
-	addCard()
+
+function initProfileInfoForm() {
+	createProfileInfoForm();
+	profileInfoFormInteractions();
 }
 
+function createProfileInfoForm() {
+	const modal = document.getElementById("settingsModal").querySelector(".modal-body");
+	let form = document.createElement("div");
+	form.classList.add("covering");
+	form.innerHTML = `
+		<form id="profileForm" action="api/profile/updateprofile/" method="POST" novalidate>
+			<div class="mb-3">
+				<label for="info-form-first-name" class="col-md-3 col-form-label">First Name</label>
+				<input type="text" name="firstName" id="info-form-first-name" class="form-control" placeholder="First Name">
+				<div id="info-form-first-name-validation"></div>
+			</div>
+			<div class="mb-3">
+				<label for="info-form-last-name" class="col-md-3 col-form-label">Last Name</label>
+				<input type="text" name="lastName" id="info-form-last-name" class="form-control" placeholder="Last Name">
+				<div id="info-form-last-name-validation"></div>
+			</div>
+			<div class="mb-3">
+				<label for="info-form-email" class="col-md-3 col-form-label">Email</label>
+				<input type="email" name="email" id="info-form-email" class="form-control" placeholder="email" pattern="^[\\w\\-\\.]+@([\\w\\-]+\\.)+[\\w\\-]{2,4}$">
+				<div id="info-form-email-validation"></div>
+			</div>
+			<button type="submit" class="btn btn-primary" id="info-form-save">Save changes</button>
+			<button type="button" class="btn btn-light" id="info-form-cancel">Cancel</button>
+		</form>
+	`
+	const currFirstName = document.getElementById("info-first-name").querySelector(".user-dependent").innerText;
+	const currLastName = document.getElementById("info-last-name").querySelector(".user-dependent").innerText;
+	const currEmail = document.getElementById("info-email").querySelector(".user-dependent").innerText;
+	modal.appendChild(form);
+	setTimeout(() => {form.classList.add("show");}, 25);
+	if (currFirstName) {
+		document.getElementById("info-form-first-name").setAttribute('value', currFirstName);
+	}
+	if (currLastName) {
+		document.getElementById("info-form-last-name").setAttribute('value', currLastName);
+	}
+	if (currEmail) {
+		document.getElementById("info-form-email").setAttribute('value', currEmail);
+	}
+	if (document.getElementById("auth-type").querySelector(".user-dependent").innerText.includes("42")) {
+		const emailField = document.getElementById("info-form-email");
+		emailField.disabled = true;
+		emailField.classList.add(".form-control-disabled");
+	}
+	return form;
+}
+
+function profileInfoFormInteractions()
+{
+	const modifyProfileForm = document.getElementById("profileForm");
+	const btnCancel = document.getElementById("info-form-cancel");
+
+	btnCancel.addEventListener("click", function () {
+		closeCovering(modifyProfileForm.parentElement);
+	})
+
+	modifyProfileForm.addEventListener("submit", async function (e) {
+		e.preventDefault();
+		if (profileInfoChanged()) {
+			if (profileInfoFormIsValid(modifyProfileForm)) {
+				let data;
+				if (document.getElementById("info-form-email").disabled) {
+					document.getElementById("info-form-email").disabled = false;
+					data = new FormData(e.target);
+					document.getElementById("info-form-email").disabled = true;
+				} else {
+					data = new FormData(e.target);
+				}
+				const url = e.target.action;
+				const body = {
+					'email': data.get('email'),
+					'first_name': data.get('firstName'),
+					'last_name': data.get('lastName'),
+				}
+				await sendUpdateProfileRequest(url, body)
+			}
+		} else {
+			closeCovering(modifyProfileForm.parentElement);
+		}
+	})
+}
+
+function profileInfoChanged() {
+	const currFirstName = document.getElementById("info-first-name").querySelector(".user-dependent").innerText;
+	const currLastName = document.getElementById("info-last-name").querySelector(".user-dependent").innerText;
+	const currEmail = document.getElementById("info-email").querySelector(".user-dependent").innerText;
+	const formFirstName = document.getElementById("info-form-first-name").value;
+	const formLastName = document.getElementById("info-form-last-name").value;
+	const formEmail = document.getElementById("info-form-email").value;
+	if (currFirstName != formFirstName || currLastName != formLastName || currEmail != formEmail)
+		return true;
+	return false;
+}
+
+function profileInfoFormIsValid() {
+	const currFirstName = document.getElementById("info-first-name").querySelector(".user-dependent").innerText;
+	const currLastName = document.getElementById("info-last-name").querySelector(".user-dependent").innerText;
+	const currEmail = document.getElementById("info-email").querySelector(".user-dependent").innerText;
+	const firstNameField = document.getElementById("info-form-first-name");
+	const firstNameValidation = document.getElementById("info-form-first-name-validation");
+	const lastNameField = document.getElementById("info-form-last-name");
+	const lastNameValidation = document.getElementById("info-form-last-name-validation");
+	const emailField = document.getElementById("info-form-email");
+	const emailValidation = document.getElementById("info-form-email-validation");
+	let toggle = true;
+
+	if (currFirstName != firstNameField.value && firstNameField.value.length > 35) {
+		firstNameField.classList.add("is-invalid")
+		firstNameValidation.classList.add("invalid-feedback")
+		firstNameValidation.innerText = "First name is too long"
+		toggle = false;
+	}
+	if (currLastName != lastNameField.value && lastNameField.value.length > 35) {
+		lastNameField.classList.add("is-invalid")
+		lastNameValidation.classList.add("invalid-feedback")
+		lastNameValidation.innerText = "Last name is too long"
+		toggle = false;
+	}
+	if (currEmail != emailField.value && (!emailField.validity.valid || emailField.value.length < 3)) {
+		emailField.classList.add("is-invalid")
+		emailValidation.classList.add("invalid-feedback")
+		emailValidation.innerText = "Email is not valid"
+		toggle = false;
+	}
+	return toggle;
+}
+
+// PASSWORD AND AUTHENTICATION
 function updatePassAuth(data) {
 	updateAuthType(data);
 	updateOtpToggle(data);
